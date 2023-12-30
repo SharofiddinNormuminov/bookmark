b5fefd1f268da92607e79f42bc8318f59202c57e
Sharofiddin sass <sharofiddinnormuminov@gmail.com>
2023-12-30 23:23:05 +0500

    third commit

--- script.js
+++ script.js
@@ -7,25 +7,20 @@ const featuresslide2 = document.querySelector("#slide2");
 const featuresslide3 = document.querySelector("#slide3");
 
 featuresTab1.addEventListener("click", () => {
-    featuresslide1.classList.remove("hidden")
-    featuresslide2.classList.add("hidden")
-    featuresslide3.classList.add("hidden")
-
-})
+  featuresslide1.classList.remove("hidden");
+  featuresslide2.classList.add("hidden");
+  featuresslide3.classList.add("hidden");
+});
 featuresTab2.addEventListener("click", () => {
-    featuresslide2.classList.remove("hidden")
-    featuresslide1.classList.add("hidden")
-    featuresslide3.classList.add("hidden")
-
-})
+  featuresslide2.classList.remove("hidden");
+  featuresslide1.classList.add("hidden");
+  featuresslide3.classList.add("hidden");
+});
 featuresTab3.addEventListener("click", () => {
-    featuresslide3.classList.remove("hidden")
-    featuresslide1.classList.add("hidden")
-    featuresslide2.classList.add("hidden")
-
-})
-
-
+  featuresslide3.classList.remove("hidden");
+  featuresslide1.classList.add("hidden");
+  featuresslide2.classList.add("hidden");
+});
 
 accordionItems.forEach((accordion) => {
   accordion.addEventListener("click", () => {
